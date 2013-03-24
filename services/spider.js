891a16526705532e1e2d807be7d74a558ff54537
OpenShift guest <burlak@psu.karelia.ru>
2013-03-24 10:24:29 -0400

    start creat application

--- services/spider.js
+++ services/spider.js
@@ -5,6 +5,7 @@
  *  Run as serveice, in crontab
  */
 var http = require('http'),
+    bus = require('../modules/bus'),
     async = require('async'),
     dm = require('domain').create(),
     stringjs = require('string'), host;
@@ -16,8 +17,7 @@ function send(opts, data, next) {
         res.setEncoding('utf8');
         
         res.on('data', function (chunk) {
-            page += chunk;
-        });
+            page += chunk;        });
         
         res.on('end', function() {
             next(null, page);
@@ -57,40 +57,69 @@ function process(item) {
     return function(next) {
 
         send(opts, null, function(err, res) {
-            next(null, stringjs(res).stripTags().s);      
+            var document = {info: item, text: stringjs(res).stripTags().s}
+            
+            bus.emit('spider:document', document);
+            next(null, document);      
         });
         
     };
     
 }
 
-function prepare(data) {
-    data.DateFrom = new Date().toISOString();
-    return JSON.stringify(data);
+function done(documents){
+    bus.emit('spider:complete', documents.length);
 }
 
+exports.run = function(cfg, done) {
+    host = cfg.url;
 
-exports.run = function(data, next) {
-    host = data.url;
+    function prepare(data, cat) {
+        data.DateFrom = new Date(Date.now() - cfg.offset).toISOString();
+        data.StatDisputeCategory = cat && cat.toString();
+        return JSON.stringify(data);
+    }
+ 
+    function exec(cat) {
+   
+        return function(next) {
+            var query = prepare(cfg.search, cat);
+        
+            search(query, function(err, res) {
+                
+                if (err) {
+                    next(err, null);
+                    return;
+                }
+
+                if (!res.Success) {
+                    next(null, []); 
+                    return;
+                }
+               
+                async.parallel(res.Result.Items.map(process), next);
+            });
+            
+        };  
     
+    }
+ 
     dm.run(function() {
         
-        search(prepare(data.search), function(err, res) {
-            
-            if (err) {
-                console.log('Search error:', err);
-                return;
-            }
-            
-            async.parallel(res.Result.Items.map(process), next);
+        var tasks = {},
+            cts = cfg.categories;
+        
+        cts.forEach(function(ct) {
+            tasks[ct] = exec(ct);     
         });
         
+        async.parallel(tasks, done);
     }); 
     
           
     dm.on('error', function(err) {
-        next(err, null);
-        console.error('domain services/spider: ', err);  
+        bus.emit('app:error', 'domain services/spider: ' + err);  
+        done(err, null);
     });
     
 };
